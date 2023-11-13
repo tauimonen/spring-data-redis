ecb0bebb9b847b1bbb13e4499eb27ff17ed050ab
vgtoui <toni.uimonen@tuni.fi>
2023-11-13 11:22:52 +0200

    Create a Spring project and a product entity

---
+++ src/main/java/com/tau/redis/entity/Product.java
@@ -0,0 +1,23 @@
+package com.tau.redis.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@RedisHash("Product")
+public class Product {
+
+    @Id
+    private int id;
+
+    private String name;
+
+    private int qty;
+
+    private long price;
+}
